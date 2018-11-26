be1067bc4e5bbce387b291da19264e51fc2eb16e
Mateusz Zalewski <zaleslaw@gmail.com>
2018-11-27 00:46:47 +0100

    Register Element service

--- src/ServiceContainer/PageObjectExtension.php
+++ src/ServiceContainer/PageObjectExtension.php
@@ -6,11 +6,13 @@ namespace FriendsOfBehat\PageObjectExtension\ServiceContainer;
 
 use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
+use FriendsOfBehat\PageObjectExtension\Element\Element;
 use FriendsOfBehat\PageObjectExtension\Page\Page;
 use FriendsOfBehat\PageObjectExtension\Page\SymfonyPage;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\DependencyInjection\Parameter;
@@ -26,27 +28,15 @@ final class PageObjectExtension implements Extension
     public function load(ContainerBuilder $container, array $config): void
     {
         try {
+            /** @var ContainerInterface $scenarioContainer */
             $scenarioContainer = $container->get('fob_context_service.service_container.scenario');
         } catch (ServiceNotFoundException $exception) {
             return;
         }
 
-        $pageDefinition = new Definition(Page::class, [
-            new Reference('mink.default_session'),
-            new Parameter('__behat__.mink.parameters')
-        ]);
-        $pageDefinition->setAbstract(true);
-        $pageDefinition->setPublic(false);
-
-        $scenarioContainer->setDefinition('fob_page_object.behat.page', $pageDefinition);
-
-        $symfonyPageDefinition = new ChildDefinition('fob_page_object.behat.page');
-        $symfonyPageDefinition->setClass(SymfonyPage::class);
-        $symfonyPageDefinition->addArgument(new Reference('__symfony_shared__.router'));
-        $symfonyPageDefinition->setAbstract(true);
-        $symfonyPageDefinition->setPublic(false);
-
-        $scenarioContainer->setDefinition('fob_page_object.behat.symfony_page', $pageDefinition);
+        $pageDefinition = $this->registerPageDefinition($scenarioContainer);
+        $this->registerSymfonyPageDefinition($scenarioContainer, $pageDefinition);
+        $this->registerElementDefinition($scenarioContainer);
     }
 
     public function initialize(ExtensionManager $extensionManager): void
@@ -59,5 +49,42 @@ final class PageObjectExtension implements Extension
 
     public function process(ContainerBuilder $container): void
     {
+    }
+
+    private function registerPageDefinition(ContainerInterface $scenarioContainer): Definition
+    {
+        $pageDefinition = new Definition(Page::class, [
+            new Reference('mink.default_session'),
+            new Parameter('__behat__.mink.parameters')
+        ]);
+        $pageDefinition->setAbstract(true);
+        $pageDefinition->setPublic(false);
+
+        $scenarioContainer->setDefinition('fob_page_object.behat.page', $pageDefinition);
+
+        return $pageDefinition;
+    }
+
+    private function registerSymfonyPageDefinition(ContainerInterface $scenarioContainer, Definition $pageDefinition): void
+    {
+        $symfonyPageDefinition = new ChildDefinition('fob_page_object.behat.page');
+        $symfonyPageDefinition->setClass(SymfonyPage::class);
+        $symfonyPageDefinition->addArgument(new Reference('__symfony_shared__.router'));
+        $symfonyPageDefinition->setAbstract(true);
+        $symfonyPageDefinition->setPublic(false);
+
+        $scenarioContainer->setDefinition('fob_page_object.behat.symfony_page', $pageDefinition);
+    }
+
+    private function registerElementDefinition(ContainerInterface $scenarioContainer): void
+    {
+        $elementDefinition = new Definition(Element::class, [
+            new Reference('mink.default_session'),
+            new Parameter('__behat__.mink.parameters')
+        ]);
+        $elementDefinition->setAbstract(true);
+        $elementDefinition->setPublic(false);
+
+        $scenarioContainer->setDefinition('fob_page_object.behat.element', $elementDefinition);
     }
 }
